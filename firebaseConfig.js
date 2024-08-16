c658e86867271f6f7d57188fc7ce785eb7657af0
Leticia Cristina <133662502+Leticianha@users.noreply.github.com>
2024-08-16 01:23:08 -0300

    .

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCvb3nSYrcJ_JexbkO_c6FDgrggOtAjptw",
-    authDomain: "nexus-47d4e.firebaseapp.com",
-    projectId: "nexus-47d4e",
-    storageBucket: "nexus-47d4e.appspot.com",
-    messagingSenderId: "322156513563",
-    appId: "1:322156513563:web:16cfcb8d177cc1d1b1abbe",
-    measurementId: "G-81FXBZR2KC"
+    // colocar a chave do API
 };
 
 const app = initializeApp(firebaseConfig);
